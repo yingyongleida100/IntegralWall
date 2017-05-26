dd80a3a0fc58c546ad776470917746599f33e9f3
wangyongchang@ann9.com <Wyc2017@ann9>
2017-05-26 02:32:27 -0400

    Init Programs

--- Web/ApiWeb.py
+++ Web/ApiWeb.py
@@ -28,9 +28,21 @@ def processUserClick(env):
 def processCheckIDFAs(env):
     result = {}
     try:
-        query = parse_qs(env['QUERY_STRING'])
-        idfas = query.get('idfa', [''])[0]
-        appid = query.get('appid', [''])[0]
+        if env['REQUEST_METHOD'].upper() == 'POST':
+            
+            try:
+                request_body_size = int(env.get('CONTENT_LENGTH', 0))
+            except(ValueError):
+                request_body_size = 0
+
+            request_body = env['wsgi.input'].read(request_body_size)
+            d = parse_qs(request_body)
+            idfas = d.get('idfa', [''])[0]
+            appid = d.get('appid', [''])[0]
+        elif env['REQUEST_METHOD'].upper() == 'GET':
+            query = parse_qs(env['QUERY_STRING'])
+            idfas = query.get('idfa', [''])[0]
+            appid = query.get('appid', [''])[0]
         appAgentFacade = AppAgentFacade.AppAgentFacade()
         result = appAgentFacade.CheckIDFAs(idfas, appid)
     except:
